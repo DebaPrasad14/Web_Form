35337d9cbf9e6bc328a3ea0f2c8867c6293c7bed
unknown <mdebaprasad14@gmail.com>
2019-05-16 01:01:12 +0530

    adding home page & about page

--- webForm/webApp/admin.py
+++ webForm/webApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import UserField
 
 # Register your models here.
+
+admin.site.register(UserField)
--- webForm/webApp/views.py
+++ webForm/webApp/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'html/base.html',{})
+    return render(request, 'html/index.html',{})
+
+
+def about(request):
+    my_name = "Deba Prasad"
+    company = "AMDOCS DVCI, PUNE"
+    address = "PUNE"
+    context = {"name":my_name, "company":company, "address":address}
+    return render(request,'html/about.html', context)
